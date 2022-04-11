509eb54cb06b426c3750958260974aa4edacfbd2
orhanacikgoz <orhanacikgozz@outlook.com>
2022-04-11 18:15:21 +0300

    Circle

---
+++ src/Kodiz.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class Kodiz {
+    public static void main(String[] args) {
+        double r, alan, cevre, pi=3.14;
+        // String alan1, cevre1;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Dairenin yari capini giriniz: ");
+        r = inp.nextDouble();
+        alan=pi*r*r;
+        cevre=2*pi*r;
+        System.out.println("Alan: "+ alan);
+        System.out.println("Cevre: "+cevre);
+
+        //System.out.println("Alan mı bulmak istiyorsunuz cevre mi yaziniz: ");
+        //alan1=inp.next();
+        //cevre1=inp.next();
+
+     //   if (alan1="Alan"){
+        //  System.out.println(alan);
+        //  }
+    //  else {
+        //      System.out.println(cevre);
+    //   }
+
+
+
+    }
+}
